d71d81a58e1999fc14a1f8bbcedddc8b20cac1cf
dt18jjt <57010876+dt18jjt@users.noreply.github.com>
2020-04-12 13:38:45 +0100

    Start and End Screen

--- Assignment-2/Avalanche.py
+++ Assignment-2/Avalanche.py
@@ -3,6 +3,7 @@ from sprites import *
 import pygame as pg, sys
 from pygame.locals import *
 import random
+import time
 vec = pg.math.Vector2
 
 # Class for the main game
@@ -19,18 +20,19 @@ class Game:
         self.player = Player(self)
         self.freeze = Freeze()
         self.pile = Pile()
+        self.exit = pg.sprite.Group()
         self.platforms = pg.sprite.Group()
         self.snowPlatforms = pg.sprite.Group()
         self.icePlatforms = pg.sprite.Group()
         self.allPlatforms = pg.sprite.Group()
         self.snowflakes = pg.sprite.Group()
         self.font_name = pg.font.match_font(FONT_NAME)
-        self.dash = DASH_COUNT
+
 
     def new(self):
         global vec
-        self.all_sprites.add(self.player, self.freeze, self.pile)
         self.player.pos = vec(0, HEIGHT - 100)
+        self.pile.rect.center = vec(WIDTH / 2, HEIGHT + 350)
         for plat in PLATFORM_LIST:
             p = Platform(*plat)
             self.all_sprites.add(p)
@@ -43,7 +45,12 @@ class Game:
             i = Ice(*ice)
             self.all_sprites.add(i)
             self.icePlatforms.add(i)
-        self.allPlatforms.add(self.platforms,self.snowPlatforms, self.icePlatforms)
+        for exit in EXIT_LIST:
+            e = Exit(*exit)
+            self.all_sprites.add(e)
+            self.exit.add(e)
+        self.allPlatforms.add(self.platforms,self.snowPlatforms, self.icePlatforms,self.exit)
+        self.all_sprites.add(self.player, self.freeze, self.pile)
         self.run()
 
     def run(self):
@@ -54,24 +61,25 @@ class Game:
             self.events()
             self.update()
             self.draw()
-        #while not self.playing:
-            #self.overScreen()
+
 
     def update(self):
+        global vec
+        global LEVEL
         self.all_sprites.update()
         self.freeze.fall()
-        self.pile.fill()
+        # platforms moving down
+        for plat in self.allPlatforms:
+            plat.rect.y += 1
         # player on normal platform
         hits = pg.sprite.spritecollide(self.player, self.platforms, False)
         if hits:
             self.player.pos.y = hits[0].rect.top
             self.player.vel.y = -0.12
-        # player on snow platform
+        now = pg.time.get_ticks()
         snowhits = pg.sprite.spritecollide(self.player, self.snowPlatforms, False)
         for snow in self.snowPlatforms:
             if snowhits and snow.rect.y == self.player.pos.y:
-                self.player.pos.y = snowhits[0].rect.top
-                self.player.vel.y = 0
                 snow.kill()
         # player on ice platform
         icehits = pg.sprite.spritecollide(self.player, self.icePlatforms, False)
@@ -81,40 +89,30 @@ class Game:
         frozen = pg.sprite.collide_rect(self.player, self.freeze)
         if frozen:
             self.freeze.reset()
-
+            pg.time.wait(200)
+            self.player.frozentime = time.time()
             pass
         # if player reaches top 1/4 of screen
-        if self.player.rect.top <= HEIGHT / 6:
+        if self.player.rect.top <= HEIGHT / 4:
             self.player.pos.y += abs(self.player.vel.y)
             for plat in self.allPlatforms:
                 plat.rect.y += abs(self.player.vel.y)
-                #if plat.rect.top >= HEIGHT:
-                   # plat.kill()
-
-        # Die!
-        #death = pg.sprite.collide_rect(self.player, self.pile)
-        #if death:
-            #self.pile.kill()
-            #self.player.kill()
-            #self.playing = False
-
-        if self.player.rect.bottom > HEIGHT / 1.2:
+                if plat.rect.top >= HEIGHT:
+                   plat.kill()
+        # Die
+        if self.player.rect.bottom > HEIGHT/1.2:
+            death = pg.sprite.collide_rect(self.player, self.pile)
             for sprite in self.all_sprites:
                 sprite.rect.y -= max(self.player.vel.y, 10)
-                if sprite.rect.bottom < -1000:
+                if death:
+                    self.playing = False
                     sprite.kill()
 
-
-        # spawn new platforms to keep same average number
-        #while len(self.platforms) < 6:
-            #width = random.randrange(50, 100)
-           # p = Platform(random.randrange(0, WIDTH - width),
-                         #random.randrange(-60, -30),
-                         #width, 20)
-            #self.platforms.add(p)
-            #self.allPlatforms.add(p)
-            #self.all_sprites.add(p)
-
+        # Complete
+        complete = pg.sprite.spritecollide(self.player, self.exit, False)
+        if complete:
+            LEVEL += 1
+            self.completeScreen()
 
 
 
@@ -136,10 +134,45 @@ class Game:
 
 
     def startScreen(self):
-        pass
+        # game splash/start screen
+        self.screen.fill(CYAN)
+        self.draw_text("UPHILL CHILLS", 48, WHITE, WIDTH / 2, HEIGHT / 4)
+        self.draw_text("L/R Arrows to move, UP to jump", 22, WHITE, WIDTH / 2, HEIGHT / 2)
+        self.draw_text("Space to DASH",22, WHITE, WIDTH/2, HEIGHT * 5/8)
+        self.draw_text("Press a key to play", 22, WHITE, WIDTH / 2, HEIGHT * 3 / 4)
+        pg.display.flip()
+        self.wait_for_key()
 
     def overScreen(self):
-        self.screen.fill(BLACK)
+        if not self.running:
+            return
+        self.screen.fill(BLUE)
+        self.draw_text("GAME OVER", 48, WHITE, WIDTH / 2, HEIGHT / 4)
+        self.draw_text("Press a key to play again", 22, WHITE, WIDTH / 2, HEIGHT * 3 / 4)
+        pg.display.flip()
+        self.wait_for_key()
+
+    def completeScreen(self):
+        global LEVEL
+        print(LEVEL)
+        self.screen.fill(PURPLE)
+        self.draw_text("LEVEL COMPlETE", 48, WHITE, WIDTH / 2, HEIGHT / 4)
+        self.draw_text("Press a key to next level", 22, WHITE, WIDTH / 2, HEIGHT * 3 / 4)
+        pg.display.flip()
+        self.wait_for_key()
+
+
+
+    def wait_for_key(self):
+        waiting = True
+        while waiting:
+            self.clock.tick(60)
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    waiting = False
+                    self.running = False
+                if event.type == pg.KEYUP:
+                    waiting = False
 
     def draw_text(self, text, size, color, x, y):
         font = pg.font.Font(self.font_name, size)
@@ -155,9 +188,10 @@ class Game:
 
 
 g = Game()
-
+g.startScreen()
 while g.running:
     g.new()
+    g.overScreen()
 pg.quit()
 
 
--- Assignment-2/sprites.py
+++ Assignment-2/sprites.py
@@ -1,6 +1,7 @@
 from settings import *
-import random
 import pygame as pg, sys
+import random
+import time
 vec = pg.math.Vector2
 
 
@@ -18,8 +19,11 @@ class Player(pg.sprite.Sprite):
         self.vel = vec(0, 0)
         self.acc = vec(0, 0)
         self.dash = 5
-        self.cooldown = 0
         self.frozen = False
+        self.frozentime = 0
+        self.frozencooldown = 1000
+        self.dashlast = pg.time.get_ticks()
+        self.dashcooldown = 1000
 
     def jump(self):
         # jump only if standing on a platform
@@ -35,9 +39,11 @@ class Player(pg.sprite.Sprite):
         global DASH_TIMER
         global DASH_STR
         global PLAYER_ACC
+        global PLAYER_JSTR
         self.acc = vec(0, PLAYER_GRAV)
         keys = pg.key.get_pressed()
         slide = pg.sprite.spritecollide(self, self.game.icePlatforms, False)
+        now = pg.time.get_ticks()
         # if the player is on a ice platform the friction is reduced making it slide
         if slide:
             PLAYER_FRICTION = 0
@@ -47,19 +53,20 @@ class Player(pg.sprite.Sprite):
         if keys[pg.K_LEFT]:
             PLAYER_ACC = 0.5
             self.acc.x = -PLAYER_ACC
-            if keys[pg.K_SPACE] and self.dash > 0 and self.cooldown == 0:
-                self.cooldown = 1
+            if keys[pg.K_SPACE] and self.dash > 0 and now - self.dashlast >= self.dashcooldown:
                 self.dash -= 1
                 PLAYER_ACC = DASH_STR
                 self.acc.x = -PLAYER_ACC
+                self.dashlast = now
         # moves to the right
         if keys[pg.K_RIGHT]:
             PLAYER_ACC = 0.5
             self.acc.x = PLAYER_ACC
-            if keys[pg.K_SPACE] and self.dash > 0:
+            if keys[pg.K_SPACE] and self.dash > 0 and now - self.dashlast >= self.dashcooldown:
                 self.dash -= 1
                 PLAYER_ACC = DASH_STR
                 self.acc.x = PLAYER_ACC
+                self.dashlast = now
         if keys[pg.K_UP]:
             self.jump()
         # apply friction
@@ -75,19 +82,20 @@ class Player(pg.sprite.Sprite):
             self.pos.x = 15
         if self.pos.y <= 0:
             self.game.playing = False
-        # wrap around the sides of the screen
+        # when frozen
+        if time.time() - self.frozentime > 2:
+            self.image.fill(RED)
+            PLAYER_ACC = 0.5
+            PLAYER_JSTR = -25
+        else:
+            self.image.fill(BLUE)
+            PLAYER_ACC = 0.2
+            PLAYER_JSTR = -15
+            # wrap around the sides of the screen
         #if self.pos.x > WIDTH:
             #self.pos.x = 0
         #if self.pos.x < 0:
             #self.pos.x = WIDTH
-
-    def dashing(self):
-        # dash countdown
-        while self.dash <= 0:
-            self.cooldown += 0.01
-            if self.cooldown >= 10:
-                self.dash += 1
-                self.cooldown = 0
 
 
 # Class for normal platforms
@@ -136,7 +144,7 @@ class Freeze(pg.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.center = (random.randrange(10, WIDTH - 10), 0)
         self.pos = vec(random.randrange(10, WIDTH - 10), 10)
-        self.speed = random.randrange(5, 10)
+        self.speed = 5
 
     def fall(self):
         self.rect.center = self.pos
@@ -154,15 +162,26 @@ class Pile(pg.sprite.Sprite):
         global vec
         pg.sprite.Sprite.__init__(self)
         self.height = 150
-        self.image = pg.Surface((WIDTH, self.height))
-        self.image.fill(WHITE)
+        self.image = pg.Surface((WIDTH, HEIGHT))
+        self.image.fill(GREY)
         self.rect = self.image.get_rect()
-        self.rect.center = (WIDTH / 2, HEIGHT + 100)
-        self.pos = vec(0, HEIGHT + 100)
+        self.pos = vec(WIDTH / 2, HEIGHT + 300)
+        self.rect.center = (WIDTH / 2, HEIGHT + 350)
 
     def fill(self):
-        self.height += 1
-        pg.transform.scale(self.image, (WIDTH, self.height))
+        self.rect.center = self.pos
+        self.pos.y -= 1
 
+
+class Exit(pg.sprite.Sprite):
+
+    def __init__(self,x, y):
+        global vec
+        pg.sprite.Sprite.__init__(self)
+        self.image = pg.Surface((50, 70))
+        self.image.fill(PURPLE)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
 
 
